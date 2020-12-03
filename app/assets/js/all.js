4f804aeb0209888072b057b0be5be6063e464b91
carol-hsieh <chun998@gmail.com>
2020-12-03 22:47:46 +0800

    Update 2020-12-03T14:47:38.279Z

--- app/assets/js/all.js
+++
@@ -1,6 +0,0 @@
-
-// $(document).ready(function(){
-//   $(".textReply").click(function(){
-//   $(".textReplycard").toggleClass("active");   
-//     });
-//   });
---
+++ assets/js/all.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//   $(".textReply").click(function(){
+//   $(".textReplycard").toggleClass("active");   
+//     });
+//   });
+"use strict";
+//# sourceMappingURL=all.js.map
